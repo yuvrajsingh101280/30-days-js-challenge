62a0d25cd7ee1101462ca723a6025e5ce1eb5fe8
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-07-15 11:46:28 +0530

    completed the day1

---
+++ day1/index.js
@@ -0,0 +1,33 @@
+// activity 1
+// task1
+var a = 10;
+console.log(a)
+// taks 2
+let str = "hello world"
+console.log(str)
+
+// activity 2
+// task3
+const bool = true
+console.log(bool)
+
+// activity 3
+// taks 4
+const number = 10
+const string = "yuvraj"
+const boolean = false
+const obj = { name: "yuvra ", age: 10 }
+const arr = [1, 2, 3, 4, 5, 6,]
+console.log(typeof (number))
+console.log(typeof (string))
+console.log(typeof (boolean))
+console.log(typeof (obj))
+console.log(typeof (arr))
+
+
+// activity 4
+// task5
+let x = 10;
+console.log(x)
+x = 20
+console.log(x)
